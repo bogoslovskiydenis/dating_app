e973d42c796bdf85617733c3f347512ac9d3d36f
Denis Bogoslovskiy <bogoslovskiydenis@gmail.com>
2022-01-10 14:32:20 +0200

    home_screen

---
+++ lib/home/home_screen.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+  static const String routeName = '/';
+
+  static Route route() {
+    return MaterialPageRoute(
+      settings: const RouteSettings(name: routeName),
+      builder: (context) => HomeScreen(),
+    );
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CustomAppBar(),
+      body: UserCard(),
+    );
+  }
+}
+
+class UserCard extends StatelessWidget {
+  const UserCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class CustomAppBar  extends StatelessWidget with PreferredSizeWidget{
+  const CustomAppBar({
+    Key? key}):  super(key:key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Row(
+          children: [
+            Expanded(
+                child: Image.asset(
+              'assets/logo.png',
+              height: 50,
+            )),
+            Expanded(
+                flex: 2,
+                child: Text(
+                  'Dating',
+                  style: GoogleFonts.reenieBeanie(
+                    fontSize: 18,
+                    color: Color(0xFF282E4A),
+                    fontWeight: FontWeight.bold,
+                  ),
+                ))
+          ],
+        ),
+        actions: [
+          IconButton(onPressed: (){}, icon: Icon(Icons.message ,color: Theme.of(context).primaryColor,)),
+          IconButton(onPressed: (){}, icon: Icon(Icons.person ,color: Theme.of(context).primaryColor,)),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(56.0);
+}
